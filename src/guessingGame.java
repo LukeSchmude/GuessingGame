0d0f9a83e7e787f9e910e405ee1abf3744eb1488
LukeSchmude <94869446+LukeSchmude@users.noreply.github.com>
2022-06-06 14:01:06 -0500

    Initial commit

---
+++ src/guessingGame.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class guessingGame {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in); // player guess
+
+        int number = (int) (1 + 20 * Math.random());
+        int gl = 6; //guesses left = gl
+        int i, guess;
+
+        System.out.println("Hello! What is your name?");
+        String player = sc.nextLine();
+        System.out.println("Well " + player + ", I am thinking of a number between 1 and 20." + "\n" + "Take a guess");
+
+        for (i = 0; i < gl; i++) {
+            System.out.println("Guess the number");
+
+            guess = sc.nextInt();
+
+            if (number == guess) {
+                System.out.println(("Good job, ") + player + ("!") + (" You guessed my number in") + gl + (" guesses!"));
+                break;
+            } else if (number > guess && i != gl - 1) {
+                System.out.println("Your guess is too high.");
+            } else if (number < guess && i != gl - 1) {
+                System.out.println("Your guess is too low.");
+            }
+        }
+        if (i == gl) {
+            System.out.println("No more guesses" + "\n" + "You lose.");
+            System.out.println("The number was" + number);
+        }
+    }
+}
